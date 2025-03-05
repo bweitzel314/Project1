a5a3e6283fdfb27081ad326b3c7159138550ab31
bweitzel314 <bweitzel@temple.edu>
2025-03-05 12:44:46 -0500

    Stylized cards more

--- script.js
+++ script.js
@@ -13,10 +13,17 @@ function showDestinations(destinations) {
         card.classList.add('destination-card');
 
         card.innerHTML = `
-            <img src="${destination.image}" alt="${destination.name}">
+        <img src="${destination.image}" alt="${destination.name}">
+        <div class="card-content">
             <h3>${destination.name}</h3>
             <p>${destination.description}</p>
-        `;
+            <h4>Itinerary:</h4>
+            <ul>
+                ${destination.details.itinerary.map(item => `<li>${item}</li>`).join('')}
+            </ul>
+            <button class="book-button" data-id="${destination.id}">Book Now</button>
+        </div>
+    `;
 
         card.addEventListener('click', function () {
             localStorage.setItem('selectedDestination', JSON.stringify(destination));
